4e084df6fde2eed2bc3cb9bafea3760a18666392
심정현 <sraccoon@simjeonghyeon-ui-MacBookPro.local>
2021-08-21 21:03:04 +0900

    debug log

--- src/Socket/event/host.ts
+++ src/Socket/event/host.ts
@@ -3,14 +3,17 @@ import { RoomManager } from 'Room/RoomManger';
 
 export default async function (socket:Socket) {
 	socket.on('host', function (data) {
-        
 		let room = RoomManager.getInstance().addRoom();
+
 		if (data.character === 0) {
 			room.setArcheologist(socket.id);
 		} else if (data.character === 1) {
 			room.setAssistant(socket.id);
 		}
+
 		socket.join(room.getRoomId());
+        
+		console.log(RoomManager.getInstance().getGame(room.getRoomId()));
 		socket.emit('inviteCode', { roomId: room.getRoomId() });
 	});
 };
