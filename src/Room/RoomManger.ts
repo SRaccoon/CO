b35dc8fd1323915aee3ae0654d5cb0066f6d3ebb
심정현 <sraccoon@simjeonghyeon-ui-MacBookPro.local>
2021-08-21 21:45:55 +0900

    test용 코드로 변경

--- src/Room/RoomManger.ts
+++ src/Room/RoomManger.ts
@@ -1,4 +1,4 @@
-import { uuid } from 'uuidv4';
+// import { uuid } from 'uuidv4';
 import { Room } from './Room';
 
 export class RoomManager {
@@ -23,7 +23,7 @@ export class RoomManager {
 	}
 
 	public addRoom(): Room {
-		let roomId = uuid();
+		let roomId = 'test';
 		let room = new Room(roomId);
 		this.roomList.set(roomId, room);
 		return room;
