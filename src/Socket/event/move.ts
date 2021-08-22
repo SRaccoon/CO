ecf81746e5407fcd5c9268f21857197a82d25477
심정현 <sraccoon@simjeonghyeon-ui-MacBookPro.local>
2021-08-22 10:59:31 +0900

    latency logging

--- src/Socket/event/move.ts
+++ src/Socket/event/move.ts
@@ -12,7 +12,7 @@ export default async function (socket:Socket) {
 			const arrive = moment();
 			console.log('start', start.hours(), start.minutes(), start.seconds(), start.milliseconds());
 			console.log('arrive', arrive.minutes(), arrive.seconds(), arrive.milliseconds());
-			console.log('latency', arrive, start, arrive.valueOf() - start.valueOf() - 32400000);
+			console.log('latency', arrive.valueOf() - start.valueOf() + 32400000);
 			const room = RoomManager.getInstance().getGame(data.roomId);
 			room.sync(data.ball, data.wire, data.time);
 			// room.setSyncData(data.direction, data.ball, data.wire);
